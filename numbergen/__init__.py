1b1081f46ddad71471e876a3f8db237e1cdff10a
jlstevens <jlstevens[at]ed.ac.uk>
2013-11-27 01:34:09 +0000

    Added the TimeDependentValue SquareWave generator to numbergen

--- numbergen/__init__.py
+++ numbergen/__init__.py
@@ -387,6 +387,51 @@ class BoxCar(NumberGenerator, TimeDependentValue):
         else:
             return 1.0
 
+
+
+class SquareWave(NumberGenerator, TimeDependentValue):
+    """
+    Generate a square wave with 'on' periods returning 1.0 and
+    'off'periods returning 0.0 of specified duration(s). By default
+    the portion of time spent in the high state matches the time spent
+    in the low state.
+
+    The 'on' state begins after a time specified by the 'onset'
+    parameter.  The onset duration supplied must be less than the off
+    duration.
+    """
+
+    onset = param.Number(0.0, doc="""Time of onset of the first 'on'
+        state relative to time 0. Must be set to a value less than the
+        'off_duration' parameter.""")
+
+    duration = param.Number(1.0, allow_None=False, doc="""
+         Duration of the 'on' state during which a value of 1.0 is
+         returned.""")
+
+    off_duration = param.Number(default=None, allow_None=True, doc="""
+        Duration of the 'off' value state during which a value of 0.0
+        is returned. By default, this duration matches the value of
+        the 'duration' parameter.""")
+
+    def __init__(self, **params):
+        super(SquareWave,self).__init__(**params)
+
+        if self.off_duration is None:
+            self.off_duration = self.duration
+
+        if self.onset > self.off_duration:
+            raise AssertionError("Onset value needs to be less than %s" % self.onset)
+
+    def __call__(self):
+        phase_offset = (self.time_fn() - self.onset) % (self.duration + self.off_duration)
+        if phase_offset < self.duration:
+            return 1.0
+        else:
+            return 0.0
+
+
+
 class ExponentialDecay(NumberGenerator, TimeDependentValue):
     """
     Function object that provides a value that decays according to an
