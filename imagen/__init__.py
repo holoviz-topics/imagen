e0eae45056d5f64fe264b27690d0e5beafd11fc8
jlstevens <jlstevens[at]ed.ac.uk>
2014-02-12 23:05:36 +0000

    Introduced the Crop composite pattern generator to Imagen

--- imagen/__init__.py
+++ imagen/__init__.py
@@ -27,7 +27,7 @@ from param import ClassSelector
 from patterngenerator import Constant, PatternGenerator
 
 from dataviews import SheetStack
-from dataviews.sheetcoords import SheetCoordinateSystem
+from dataviews.sheetcoords import SheetCoordinateSystem, Slice
 from dataviews import boundingregion, sheetcoords # pyflakes:ignore (API import)
 
 from patternfn import gaussian,exponential,gabor,line,disk,ring,\
@@ -596,7 +596,7 @@ class Composite(PatternGenerator):
 
     def state_pop(self):
         """
-        Push the state of all generators
+        Pop the state of all generators
         """
         super(Composite,self).state_pop()
         for gen in self.generators:
@@ -623,6 +623,51 @@ class Composite(PatternGenerator):
                     for pg in generators]
         image_array = p.operator.reduce(patterns)
         return image_array
+
+
+
+class Crop(PatternGenerator):
+    """
+    Crop may be used to select a subregion of the output of another
+    pattern generator. This may be useful to eliminate possible border
+    effects that may result after applying an output function applied
+    to a pattern generator.
+
+    The bounds of the Crop object must be contained within the bounds
+    of the supplied pattern object.
+    """
+
+    pattern = param.Parameter(default=Gaussian(), doc="""
+        The pattern to be cropped.""")
+
+    bounds = boundingregion.BoundingRegionParameter(
+        default=boundingregion.BoundingBox(radius=0.2), doc="""
+        The cropping bounds used to define the subregion to be cropped.""")
+
+    def __call__(self,**params_to_override):
+        p = ParamOverrides(self,params_to_override)
+
+        if p.bounds not in p.pattern.bounds:
+            raise Exception("The crop bounds must be within the pattern bounds.")
+
+        data = p.pattern(xdensity=p.xdensity, ydensity=p.ydensity)
+        sheetcoord = SheetCoordinateSystem(p.pattern.bounds, p.xdensity, p.ydensity)
+        return Slice(p.bounds, sheetcoord).submatrix(data)
+
+    def state_push(self):
+        """
+        Push the state of the cropped pattern.
+        """
+        super(Crop,self).state_push()
+        self.pattern.state_push()
+
+    def state_pop(self):
+        """
+        Pop the state of the cropped pattern.
+        """
+        super(Crop,self).state_pop()
+        self.pattern.state_pop()
+
 
 
 
