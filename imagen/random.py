237223ccad431f98441025e5ae1898101d860d72
h-mayorquin <h.mayorquin@gmail.com>
2014-03-12 18:36:10 +0100

    whitespace correction

--- imagen/random.py
+++ imagen/random.py
@@ -337,8 +337,8 @@ class UniformRandom(RandomGenerator):
 class UniformRandomInt(RandomGenerator):
     """
     2D distribution of integer values from low to high in the in the
-    half-open interval [`low`, `high`).
-       
+    half-open interval [`low`, `high`). 
+    
     Matches semantics of numpy.random.randint.
     """
 
