cb5701463a6f49b80cd3820570d84ba60dc2c622
jlstevens <jlstevens@ed.ac.uk>
2015-03-31 19:54:40 +0100

    Bumped to version 2.0.1 due to outdated setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ for package_list in packages_to_state:
 
 setup_args.update(dict(
     name='imagen',
-    version="2.0.0",
+    version="2.0.1",
     description='Generic Python library for 0D, 1D, and 2D pattern distributions.',
     long_description=open('README.rst').read() if os.path.isfile('README.rst') else 'Consult README.rst',
     author= "IOAM",
