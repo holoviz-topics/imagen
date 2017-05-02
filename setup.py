65b77a0bd1260cffff1270cbd673f14784b4507a
jlstevens <jlstevens@ed.ac.uk>
2017-05-02 21:00:34 +0100

    Simplified setup.py

--- setup.py
+++ setup.py
@@ -81,18 +81,8 @@ def check_pseudo_package(path):
 
 
 if __name__=="__main__":
-
-    if 'IMAGEN_RELEASE' in os.environ:
-        # Make sure to create these directories and populate them before upload
-        setup_args['packages'] += ["imagen.tests", "imagen.notebooks", "imagen.notebooks.images"]
-        setup_args['package_data']['imagen.notebooks'] = ['*.ipynb']
-        setup_args['package_data']['imagen.notebooks.images'] = ['*.pgm']
-
-        if ('upload' in sys.argv) or ('sdist' in sys.argv):
-            check_pseudo_package(os.path.join('.', 'imagen', 'tests'))
-            check_pseudo_package(os.path.join('.', 'imagen', 'notebooks'))
-
-            import imagen
-            imagen.__version__.verify(setup_args['version'])
+    if ('upload' in sys.argv) or ('sdist' in sys.argv):
+        import imagen
+        imagen.__version__.verify(setup_args['version'])
 
     setup(**setup_args)
